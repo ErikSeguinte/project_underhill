d318c87e8f23f08c3257f69fd7f78cda96844e41
Erik <spamfactorx01@gmail.com>
2020-06-28 19:51:19 -0700

    adds database models

--- project_underhill/core/models.py
+++ project_underhill/core/models.py
@@ -1,12 +1,31 @@
-from sqlalchemy import Table, Column, String, Integer, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, ForeignKey, Enum
+from enum import Enum
 from .database import metadata
+from .schema import CardType
 
 users = Table(
     "users",
     metadata,
-    Column('user_id', String(8), primary_key=True),
-    Column('email', String),
+    Column("user_id", Integer, primary_key=True),
+    Column("email", String),
     Column("secret_question", String),
-    Column('password', String),
-    Column("secret_answer", String)
+    Column("password", String),
+    Column("secret_answer", String),
+)
+
+decks = Table(
+    "decks",
+    metadata,
+    Column("id", String(8), primary_key=True),
+    Column("owner_id", Integer, ForeignKey("users.id")),
+)
+
+
+cards = Table(
+    "cards",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("deck_id", String(8), ForeignKey("decks.id"), nullable=False),
+    Column("type", Enum(CardType)),
+    Column("text", String),
 )
