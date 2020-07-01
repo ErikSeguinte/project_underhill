da1f14fce39bf7257fd199b3ab141a29f8bcb55c
Erik <spamfactorx01@gmail.com>
2020-06-30 23:39:45 -0700

    recieves deck ids

--- project_underhill/routes/game.py
+++ project_underhill/routes/game.py
@@ -4,6 +4,8 @@ from ..core import crud, schema
 from ..core.security import get_random_string
 from typing import List, Optional
 
+import asyncio
+
 router = APIRouter()
 templates = Jinja2Templates(directory="project_underhill/templates")
 
@@ -14,21 +16,15 @@ async def create_game(request: Request):
     return templates.TemplateResponse("create_game.html", {"request": request})
 
 
-@router.post("/create/add_to_db", status_code=status.HTTP_201_CREATED)
-async def receive_cards():
-    pass
+@router.post("/setup")
+async def setup_game(changeling=Form(None), child=Form(None)):
+
+    strings = (process_string(changeling), process_string(child))
+
+    return {"changeling": strings[0], "child": strings[1]}
 
 
-async def process_things(things: List, deck_id: str, type: schema.CardType):
-    for thing in things:
-        if thing:
-            t = {"deck_id": deck_id, "type": type, "text": thing}
-
-            await crud.create_card(schema.CardCreate(**t))
-
-
-@router.get("/{deck_id}", response_model=List[schema.Card])
-async def get_deck(deck_id: str):
-    input_id = deck_id
-    cards = await crud.get_cards_by_user(input_id)
-    return cards
+def process_string(string: str) -> str:
+    if "/" in string:
+        string = string.split("/")[-1]
+    return string
